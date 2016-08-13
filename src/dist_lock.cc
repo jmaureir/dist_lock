a4fffd463a1fff87b8af419ebb1bbb192368c330
hartag <ahart@dim.uchile.cl>
2016-08-13 17:36:54 -0300

    Added help and version info, as well as correcting usage.

--- src/dist_lock.cc
+++ src/dist_lock.cc
@@ -9,6 +9,7 @@
  * (c) 2016      Andrew Hart
  */
 
+#include <unistd.h> #getopt
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -19,10 +20,62 @@
 #include "DistributedLock.h"
 #include "StringTokenizer.h"
 
-int usage() {
-    std::cerr << "usage: dist_lock -r {resource_name:[port]} -n {retry_max} -p {port} -- command_to_execute " <<  std::endl;   
-    return 1;
+
+int usage(std::string& name) {
+    std::cerr << "usage: " << name << " [-h] [-v] -r resource1[:count1] ";
+    std::cerr << "[-r resource2[:count2] ...]"<< std::endl;
+    std::cerr << "    [-n max_tries] [-p port] -- command_to_execute [args ...]"<< std::endl;
+	  std::cerr << std::endl;
+		std::cerr << "    Use option -h for more detailed help." << std::endl;
+		return 1;
 }
+
+int showVersion(std::string& name) {
+	    std::cerr << name << " 1.0.0" << std::endl;
+	    	return 0;
+}
+
+int showHelp(std::string& name) {
+    std::cout << name << " (Distributed Lock)" << std::endl;
+    std::cout << std::endl;
+    std::cout << "This command-line tool provides a light-weight simple-to-use distributed locking" << std::endl;
+    std::cout << "mechanism to restrict access by bash scripts  to one or more shared (or scarce)" << std::endl;
+    std::cout << "resources. It uses a UDP-based broadcast protocol to enable multiple processes" << std::endl;
+    std::cout << "to negotiate, acquire and release shared resources while ensuring that each  may" << std::endl;
+    std::cout << "obtain exclusive access to the resources it requires." << std::endl;
+    std::cout << std::endl;
+    std::cout << name << " can be run from user accounts without the need for installation and it" << std::endl;
+    std::cout << "can be used to manage access to shared resources by various processes, even if" << std::endl;
+    std::cout << "they have different owners." << std::endl;
+    std::cout << std::endl;
+    std::cout << "usage: "<< name << " [-h] [-v] -r resource1[:count1] [-r resource2[:count2] ...]" << std::endl;
+    std::cout << "    [-n max_tries] [-p port] -- command_to_execute [args ...]" << std::endl;
+    std::cout << std::endl;
+    std::cout << "-h" << std::endl;
+    std::cout << "Show this help and exit." << std::endl;
+    std::cout << std::endl;
+    std::cout << "-v" << std::endl;
+    std::cout << "Display version information and exit." << std::endl;
+    std::cout << std::endl;
+    std::cout << "-r resourcen[:countn]" << std::endl;
+    std::cout << "The name of the n-th resource and the number of processes that should be allowed" << std::endl;
+    std::cout << "to simultaneously acquire access to it. At least one resource must be specified," << std::endl;
+    std::cout << "but more than one may be specified, in which case all such resources must be" << std::endl;
+    std::cout << "acquired in order for command_to_execute to be run. The counts (count1, count2," << std::endl;
+    std::cout << "...) are optional and only one instance of the resource  is assumed to be" << std::endl;
+    std::cout << "available whenever no count is  specified." << std::endl;
+    std::cout << std::endl;
+    std::cout << "-n max-tries" << std::endl;
+    std::cout << "The number of times to try acquiring the specified resource(s) before giving up" << std::endl;
+    std::cout << "and failing. The default value is 0, which means that no limit is placed on the" << std::endl;
+    std::cout << "number of attempts to acquire the resource(s)." << std::endl;
+    std::cout << std::endl;
+    std::cout << "-p port" << std::endl;
+    std::cout << "The port to be used by the distributed lock protocol. The default" << std::endl;
+    std::cout << "port is 5000." << std::endl;
+    return 0;
+}
+
 
 int main(int argc, char **argv) {
 
@@ -34,8 +87,23 @@ int main(int argc, char **argv) {
     unsigned int retry_num = 0;   
     std::map<std::string,unsigned int> res_map;
  
-    while ((c = getopt (argc, argv, "r:n:p:")) != -1) {
+// Get executable name
+    std::string name(argv[0]);
+std::cout << name << std::endl;
+    size_t slashPos = name.rfind('/');
+    if (slashPos!=std::string::npos)
+        name = name.substr(slashPos+1);
+std::cout << name << std::endl;
+    
+// Process command-line arguments
+    while ((c = getopt (argc, argv, "hvr:n:p:")) != -1) {
         switch (c) {
+            case 'h':
+        		    return showHelp(name);
+        		    break;
+            case 'v':
+        		    return showVersion(name);
+        		    break;
             case 'r':
                 if (optarg!=NULL && strlen(optarg) > 0 ) {
                     resource = std::string(optarg);
@@ -53,7 +121,7 @@ int main(int argc, char **argv) {
                     }
 
                 } else {
-                    return usage();
+                    return usage(name);
                 }
                 break;
             case 'n':
@@ -68,7 +136,7 @@ int main(int argc, char **argv) {
                 break;
 
             default:
-                return usage();
+                return usage(name);
         }
     }
 
@@ -83,19 +151,19 @@ int main(int argc, char **argv) {
             std::string resource = (*it).first;
             unsigned int count  = (*it).second;
             if (!dl->defineResource(resource,count)) {
-                std::cerr << "error defininf resource " << resource << std::endl;
+                std::cerr << "DistributedLock: error registering resource " << resource << "." << std::endl;
                 return 4;
             }
         }
 
     } catch(Exception& e) {
-        std::cerr << "DistributedLock initialization error" << std::endl;
+        std::cerr << "DistributedLock: initialization error." << std::endl;
         return 3;
     }
 
     // check resource name is provided
     if (resource == "") {
-        return usage();
+        return usage(name);
     }
 
     // check command to execute
@@ -108,16 +176,16 @@ int main(int argc, char **argv) {
             }
  
         } else {
-            std::cerr << "no command given" << std::endl;
+            std::cerr << "No command specified." << std::endl;
             return 2;
         }
     } else {
-        return usage();
+        return usage(name);
     }
  
     if (dl->adquire(resource)) {
-        std::cout << "resource adquired!" << std::endl;
-        std::cout << "executing: " << cmd.str() << std::endl;
+        std::cout << "Resource acquired!" << std::endl;
+        std::cout << "Executing: " << cmd.str() << std::endl;
         pid_t pid;
 
         pid = fork();
@@ -126,11 +194,11 @@ int main(int argc, char **argv) {
             int r = execl("/bin/sh", "sh", "-c", cmd.str().c_str(), NULL);
             return r;
         } else  if (pid < 0) {
-            std::cerr << "error forking the process" << std::endl;
+            std::cerr << "error forking the process." << std::endl;
             return 4;
         } else {
             if (waitpid (pid, &status, 0) != pid) {
-                std::cerr << "error waiting for process" << std::endl;
+                std::cerr << "error waiting for process." << std::endl;
             }
             if ( WIFEXITED(status) ) {
                 int exit_code = WEXITSTATUS(status);
@@ -139,12 +207,12 @@ int main(int argc, char **argv) {
             return EXIT_FAILURE;
         }
     } else {
-        std::cerr << "Resource not adquired." << std::endl;
+        std::cerr << "Resource(s) not acquired." << std::endl;
         return 5;
     }
 
     if (!dl->releaseAll()) {
-        std::cerr << "resource not released" << std::endl;
+        std::cerr << "resource(s) not released" << std::endl;
     }
 
     delete(dl);
