4115027396228a8cca92254f356af9e97cb71fcd
hartag <ahart@dim.uchile.cl>
2016-08-18 14:38:23 -0300

    Refactor help message.  It is still missing some minor details.

--- src/dist_lock.cc
+++ src/dist_lock.cc
@@ -26,9 +26,56 @@ const std::string usage_msg(R"(
 usage: %NAME% [-h] [-v] [-b beacon_time] [-B broadcast_network] [-p port]
     [-n max_tries] -r resource1[:count1 [-r resource2[:count2] ...] --
     command_to_execute [args ...]
-Use option -h for more detailed help.)");
-	
+)");
 
+const string help_msg(R"(
+%NAME% (Distributed Lock)
+
+SYNOPSIS
+
+This command-line tool provides a light-weight simple-to-use distributed locking
+mechanism to restrict access by bash scripts  to one or more shared (or scarce) 
+resources. It uses a UDP-based broadcast protocol to enable multiple processes 
+to negotiate, acquire and release shared resources while ensuring that each  may
+obtain exclusive access to the resources it requires.
+
+%NAME% can be run from user accounts without the need for installation and
+it can be used to manage access to shared resources by various processes, even
+they have different owners.
+
+%USAGE%
+
+OPTIONS
+
+-h
+Show this help and exit.
+
+-v
+Display version information and exit.
+
+-r resourcen[:countn]
+The name of the n-th resource and the number of processes that should be allowed 
+to simultaneously acquire access to it. At least one resource must be specified, 
+but more than one may be specified, in which case all such resources must be 
+acquired in order for command_to_execute to be run. The counts (count1, count2, 
+...) are optional and only one instance of the resource  is assumed to be 
+available whenever no count is  specified.
+
+-n max-tries
+The number of times to try acquiring the specified resource(s) before giving up 
+and failing. The default value is 0, which means that no limit is placed on the 
+number of attempts to acquire the resource(s).
+
+-p port
+The port to be used by the distributed lock protocol. The default 
+port is 5000.
+
+-b beacon_time
+Interval in milliseconds between beacon packets.  The default is ?.
+
+-B broadcast_network
+Address of network to use for communication.  The default is ?.
+)");
 std::string& stringReplace(std::string& s, const std::string& from, const std::string& to)
 {
     if (!from.empty())
@@ -41,6 +88,7 @@ std::string& stringReplace(std::string& s, const std::string& from, const std::s
 int usage(std::string& name) {
     std::string msg(usage_msg);
     msg = stringReplace(msg, "%NAME%", name);	
+    msg = msg+"\nUse option -h for more detailed help.\n";
     std::cerr << msg << std::endl;
 		return 1;
 }
@@ -51,44 +99,10 @@ int showVersion(std::string& name) {
 }
 
 int showHelp(std::string& name) {
-    std::cout << name << " (Distributed Lock)" << std::endl;
-    std::cout << std::endl;
-    std::cout << "This command-line tool provides a light-weight simple-to-use distributed locking" << std::endl;
-    std::cout << "mechanism to restrict access by bash scripts  to one or more shared (or scarce)" << std::endl;
-    std::cout << "resources. It uses a UDP-based broadcast protocol to enable multiple processes" << std::endl;
-    std::cout << "to negotiate, acquire and release shared resources while ensuring that each  may" << std::endl;
-    std::cout << "obtain exclusive access to the resources it requires." << std::endl;
-    std::cout << std::endl;
-    std::cout << name << " can be run from user accounts without the need for installation and it" << std::endl;
-    std::cout << "can be used to manage access to shared resources by various processes, even if" << std::endl;
-    std::cout << "they have different owners." << std::endl;
-    std::cout << std::endl;
-
-    usage(name);
-
-    std::cout << std::endl;
-    std::cout << "-h" << std::endl;
-    std::cout << "Show this help and exit." << std::endl;
-    std::cout << std::endl;
-    std::cout << "-v" << std::endl;
-    std::cout << "Display version information and exit." << std::endl;
-    std::cout << std::endl;
-    std::cout << "-r resourcen[:countn]" << std::endl;
-    std::cout << "The name of the n-th resource and the number of processes that should be allowed" << std::endl;
-    std::cout << "to simultaneously acquire access to it. At least one resource must be specified," << std::endl;
-    std::cout << "but more than one may be specified, in which case all such resources must be" << std::endl;
-    std::cout << "acquired in order for command_to_execute to be run. The counts (count1, count2," << std::endl;
-    std::cout << "...) are optional and only one instance of the resource  is assumed to be" << std::endl;
-    std::cout << "available whenever no count is  specified." << std::endl;
-    std::cout << std::endl;
-    std::cout << "-n max-tries" << std::endl;
-    std::cout << "The number of times to try acquiring the specified resource(s) before giving up" << std::endl;
-    std::cout << "and failing. The default value is 0, which means that no limit is placed on the" << std::endl;
-    std::cout << "number of attempts to acquire the resource(s)." << std::endl;
-    std::cout << std::endl;
-    std::cout << "-p port" << std::endl;
-    std::cout << "The port to be used by the distributed lock protocol. The default" << std::endl;
-    std::cout << "port is 5000." << std::endl;
+    std::string msg(help_msg);
+    msg = stringReplace(msg, "%USAGE%", usage_msg);	
+    msg = stringReplace(msg, "%NAME%", name);	
+		std::cout << msg << std::endl;
     return 0;
 }
 
